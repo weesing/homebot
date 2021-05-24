39918df795376af98615ecea6966bbf5ea268745
WeeSing <posttomichael@gmail.com>
2021-05-24 14:19:56 +0800

    Formatting

--- src/googleassistant/assistantHelper.js
+++ src/googleassistant/assistantHelper.js
@@ -2,29 +2,29 @@ import { GoogleAssistant } from './googleassistant';
 import _ from 'lodash';
 import { cfg } from '../configLoader';
 import util from 'util';
+import logger from '../common/logger';
 
 export class GoogleAssistantHelper {
-    constructor() {
-        let deviceCredentials = _.get(cfg, `googleapi.devicecredentials`);
-        let CREDENTIALS = {
-            client_id: deviceCredentials.client_id,
-            client_secret: deviceCredentials.client_secret,
-            refresh_token: deviceCredentials.refresh_token,
-            type: "authorized_user"
-        };
-        this.assistant = new GoogleAssistant(CREDENTIALS);
-    }
+  constructor() {
+    let deviceCredentials = _.get(cfg, `googleapi.devicecredentials`);
+    let CREDENTIALS = {
+      client_id: deviceCredentials.client_id,
+      client_secret: deviceCredentials.client_secret,
+      refresh_token: deviceCredentials.refresh_token,
+      type: 'authorized_user'
+    };
+    this.assistant = new GoogleAssistant(CREDENTIALS);
+  }
 
-    async broadcast(message) {
-        let result = await this.assistant.assist(`Broadcast ${message}`);
-        console.log(`Result -`);
-        console.log(result);
-    }
+  async broadcast(message) {
+    let result = await this.assistant.assist(`Broadcast ${message}`);
+    logger.info(result);
+  }
 
-    async device(command, device) {
-        const commandMsg = `${command} the ${device}`;
-        console.log(`Sending command to assistant '${commandMsg}'`);
-        let result = await this.assistant.assist(commandMsg);
-        console.log(`Result - ${util.inspect(result, {depth:10})}`);
-    }
+  async device(command, device) {
+    const commandMsg = `${command} the ${device}`;
+    logger.info(`Sending command to assistant '${commandMsg}'`);
+    let result = await this.assistant.assist(commandMsg);
+    logger.info(result);
+  }
 }
