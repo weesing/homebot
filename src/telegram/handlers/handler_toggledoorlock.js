1d0dc673046bd27fc39a5c08b942f774c652a882
Yap Wee Sing <posttomichael@gmail.com>
2021-07-01 19:28:35 +0800

    Added port for door lock API.

--- src/telegram/handlers/handler_toggledoorlock.js
+++ src/telegram/handlers/handler_toggledoorlock.js
@@ -9,10 +9,11 @@ export class HandlerToggleDoorlock extends HandlerBase {
     await this.sendMessage({ context, msg: `Toggling Door Lock...` });
     const doorlockProtocol = _.get(cfg, `doorlock.protocol`);
     const doorlockURL = _.get(cfg, `doorlock.url`);
+    const doorlockPort = _.get(cfg, `doorlock.port`);
     const doorlockAPIPath = _.get(cfg, `doorlock.api_path`);
     const apiKey = _.get(cfg, `doorlock.api_key`);
     const response = await axios.post(
-      `${doorlockProtocol}://${doorlockURL}${doorlockAPIPath}`,
+      `${doorlockProtocol}://${doorlockURL}:${doorlockPort}${doorlockAPIPath}`,
       null,
       { headers: { api_key: apiKey } }
     );
