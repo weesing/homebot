c70f955426c540b56d9e3371de93c68c0b9b1df2
Yap Wee Sing <posttomichael@gmail.com>
2021-10-08 14:42:39 +0800

    Formatting

--- src/telegram/handlers/handler_crowd.js
+++ src/telegram/handlers/handler_crowd.js
@@ -9,22 +9,28 @@ export class HandlerCrowd extends HandlerBase {
     const spaceoutLib = new SpaceoutLib();
     const data = await spaceoutLib.getData();
 
-    var crowded = _.orderBy(data.filter((facility) => facility.band > 1), ['band', 'name'], ['desc', 'asc']);
+    var crowded = _.orderBy(
+      data.filter((facility) => facility.band > 1),
+      ['band', 'name'],
+      ['desc', 'asc']
+    );
     let crowdedStr = crowded
       .map(
         (facility) =>
-          `${facility.band > 2 ? '\u{1F7E4}' : '\u{1F7E2}'} ${facility.band > 2 ? '**' + facility.name + '**' : facility.name} (${moment(facility.createdAt).format('hh:mm A')})`
+          `${facility.band > 2 ? '\u{1F7E4}' : '\u{1F7E2}'} ${
+            facility.band > 2 ? '**' + facility.name + '**' : facility.name
+          } (${moment(facility.createdAt).format('hh:mm A')})`
       )
       .toString();
     const opts = {
       parse_mode: 'MarkdownV2'
     };
     defaultLogger.info(crowdedStr);
-    defaultLogger.info(typeof(crowdedStr));
+    defaultLogger.info(typeof crowdedStr);
     crowdedStr = crowdedStr
-        .replace(/\(/g, `\\(`)
-        .replace(/\)/g, `\\)`)
-        .replace(/\,/g, `\n`);
+      .replace(/\(/g, `\\(`)
+      .replace(/\)/g, `\\)`)
+      .replace(/\,/g, `\n`);
     defaultLogger.info(crowdedStr);
     let msg = `${crowdedStr}\n\n[SpaceOut](https://www.spaceout.gov.sg)`;
     this.sendMessage({ context, msg, opts });
