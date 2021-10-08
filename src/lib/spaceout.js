aaa30ded8d4afa18f3e13dce24e0b406043f31e2
Yap Wee Sing <posttomichael@gmail.com>
2021-10-08 10:33:24 +0800

    Spaceout sample data. Retrieving real-time data.

--- src/lib/spaceout.js
+++ src/lib/spaceout.js
@@ -5,10 +5,21 @@ import cfg from '../configLoader';
 
 export class SpaceoutLib {
   async getData() {
-    const url = cfg.spaceout.url;
-    const data = await axios.post(url).then((response) => {
-       return response.data.data.facilities;
+    const url = `${cfg.spaceout.url}?query=${cfg.spaceout.paramsAll}`;
+    const data = await axios.post(url).then(response => {
+      return response.data.data.facilities;
     });
+    for (const facility of data) {
+      // Get each real-time data
+      let facUrl = `${
+        cfg.spaceout.url
+      }?query=${cfg.spaceout.paramsFacility.replace('${id}', facility.id)}`;
+      const facData = await axios.post(facUrl).then(response => {
+        return response.data.data.facility
+      });
+      facility.band = facData.band;
+      facility.createdAt = facData.createdAt;
+    }
     return data;
   }
 }
