8123aabae2ed92a49d5641b36967c753ce0fc687
Wee Sing <posttoweesing@gmail.com>
2024-06-16 12:02:45 +0800

    Skeleton

--- src/lib/traffic_analyzer_server/gunicorn.conf.py
+++ src/lib/traffic_analyzer_server/gunicorn.conf.py
@@ -1,2 +1,2 @@
 bind = "0.0.0.0:8082"  # Host and port
-workers = 2  # Number of worker processes (adjust as needed)
+workers = 1  # Number of worker processes (adjust as needed)
--- src/lib/traffic_analyzer_server/traffic_analyzer.py
+++ src/lib/traffic_analyzer_server/traffic_analyzer.py
@@ -1,4 +1,46 @@
 from flask import Flask, request
+import glob
+import json
+import os
+from dotenv import load_dotenv,find_dotenv
+from langchain.prompts.prompt import PromptTemplate
+from langchain.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser, JsonOutputParser
+import google.generativeai as googgenai
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+print(find_dotenv())
+load_dotenv(find_dotenv())
+
+def init_llm():
+  # Google API. https://python.langchain.com/v0.2/docs/integrations/platforms/google/
+  # Need to define GOOGLE_API_KEY in .env
+  llm = ChatGoogleGenerativeAI(
+      model='gemini-1.5-flash', 
+      temperature=0,
+      google_api_key=os.getenv("GOOGLE_AI_API_KEY")
+  )
+
+  # Use HuggingFace model
+  # llm = init_huggingface()
+
+  return llm
+
+llm = init_llm()
+
+def exec_process_gpt(system_prompt, user_prompt, file_path=""):
+  chat_prompt_template = ChatPromptTemplate.from_messages(
+      [
+          ("system", system_prompt),
+          ("user", user_prompt)
+      ]
+  )
+
+  messages = chat_prompt_template.format_messages(file_path=file_path)
+  chain = ChatPromptTemplate.from_messages(messages) | llm | StrOutputParser()
+  res = chain.invoke(input={})
+  ans = res.strip()
+  return ans
 
 app = Flask(__name__)
 
@@ -9,8 +51,9 @@ def analyze():
   # For example:
   data = request.json
   print(f"Received data: {data}")
+  system_prompt=""
+  user_prompt=""
+  file_path=""
   # Process the data and return a response
+  # result = exec_process_gpt(system_prompt=system_prompt, user_prompt=user_prompt, file_path=file_path)
   return "Data received successfully!", 200
-
-if __name__ == "__main__":
-  app.run(host="0.0.0.0", port=8082)
